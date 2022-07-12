9721b1dbdcfc04120bee14aa6672b31431e3db9b
Arthur Bleil <78227085+arthurbleilzup@users.noreply.github.com>
2022-07-12 17:06:01 -0300

    feat: sample cart page (#6)
    * feat: payment page
    
    Signed-off-by: Arthur Bleil <arthur.bleil@zup.com.br>
    
    * feat: address page
    
    Signed-off-by: Arthur Bleil <arthur.bleil@zup.com.br>
    
    * feat: cart page
    
    Signed-off-by: Arthur Bleil <arthur.bleil@zup.com.br>

--- sample/src/nimbus/components/button.tsx
+++ sample/src/nimbus/components/button.tsx
@@ -1,13 +1,14 @@
-import { NimbusJSX, FC, Actions } from '@zup-it/nimbus-backend-core'
+import { NimbusJSX, FC, Actions, Operation } from '@zup-it/nimbus-backend-core'
 import { WithStyle } from '@zup-it/nimbus-backend-core/model/component'
-import { InterpolatedText } from '@zup-it/nimbus-backend-core/types'
+import { Expression, InterpolatedText } from '@zup-it/nimbus-backend-core/types'
 import { customComponentNamespace } from './shared'
 
 export interface ButtonProps extends WithStyle {
   onPress?: Actions,
   children: InterpolatedText,
+  enabled?: Expression<boolean> | Operation<boolean>,
 }
 
-export const Button: FC<ButtonProps> = ({ id, style, ...props }) => (
-  <component id={id} namespace={customComponentNamespace} name="button" properties={{ ...style, ...props } }></component>
+export const Button: FC<ButtonProps> = ({ id, style, enabled = true, ...props }) => (
+  <component id={id} namespace={customComponentNamespace} name="button" properties={{ ...style, enabled, ...props } } />
 )
