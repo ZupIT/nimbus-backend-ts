79dcdcc4ad84949563c02cb441a45e2d5caf14c9
Arthur Bleil <arthur.bleil@zup.com.br>
2022-07-20 17:15:34 -0300

    enhancement: address and payment pages refined
    Signed-off-by: Arthur Bleil <arthur.bleil@zup.com.br>

--- sample/src/nimbus/screens/payment.tsx
+++ sample/src/nimbus/screens/payment.tsx
@@ -1,16 +1,31 @@
-import { NimbusJSX, createState } from '@zup-it/nimbus-backend-core'
-import { Screen, ScreenRequest } from '@zup-it/nimbus-backend-express'
+import { NimbusJSX, createState, FC, Expression } from '@zup-it/nimbus-backend-core'
+import { Screen } from '@zup-it/nimbus-backend-express'
 import { createOrder } from '../network/order'
-import { AddressModel, PaymentCard } from '../../models/order'
+import { PaymentCard } from '../../models/order'
 import { globalState } from '../global-state'
 import { updateCartIndicator } from '../actions'
 import { localNavigator } from '../local-navigator'
-import { Column, Row, ScreenComponent } from '@zup-it/nimbus-backend-layout'
+import { Column, Row, ScreenComponent, ScrollView, Text } from '@zup-it/nimbus-backend-layout'
 import { Button } from '../components/button'
-import { log } from '@zup-it/nimbus-backend-core/actions'
+import { log, push } from '@zup-it/nimbus-backend-core/actions'
 import { TextInput } from '../components/text-input'
+import { MapStateNode } from '@zup-it/nimbus-backend-core/model/state/types'
+import { Order } from './order'
 
-export const Payment: Screen = () => {
+type PaymentInputProps = { placeholder: string, name: keyof PaymentCard, paymentState: MapStateNode<PaymentCard> }
+const PaymentInput: FC<PaymentInputProps> = ({ placeholder, name, paymentState: state }) => {
+  const getField = (name: keyof PaymentCard) => state.get(name)
+  const setField = (name: keyof PaymentCard, value: Expression<string>) => state.get(name).set(value)
+  return (
+    <Row marginTop={4}>
+      <TextInput {...{ placeholder }} value={getField(name)} onChange={value => setField(name, value)} />
+    </Row>
+  )
+}
+
+const PaymentLabel: FC<{ text: string }> = ({ text }) => <Text size={12} weight="light" color="#666">{text}</Text>
+
+export const Payment: Screen = ({ navigator }) => {
   const cart = globalState.get('cart')
   const card = createState<PaymentCard>('card')
   const makeOrder = createOrder(
@@ -23,35 +38,39 @@ export const Payment: Screen = () => {
       onSuccess: (response) => [
         cart.set([]),
         updateCartIndicator({ numberOfElementsInCart: 0 }),
-        localNavigator.goToOrderDetails(response.get('data').get('id')),
+        globalState.get('currentOrder').set(response.get('data')),
+        navigator.popTo(Order)
       ],
       onError: response => log({ message: response.get('data').get('error').toString(), level: 'Error' })
     },
   )
 
-  const createInput = (placeholder: string, name: keyof PaymentCard, shouldExpand: boolean = true) => (
-    <TextInput
-      value={card.get(name)}
-      placeholder={placeholder}
-      onChange={value => card.get(name).set(value)}
-      margin={8}
-      { ...(shouldExpand ? { flex: 1 } : {}) }
-    />
-  )
-
   return (
     <ScreenComponent title="Payment" state={card}>
-      <Column flex={1} marginTop={12} marginHorizontal={12} mainAxisAlignment="spaceBetween">
-        <Row>
-          {createInput('Card number', 'number', false)}
-          <Row>
-            {createInput('MM/YY', 'expirationDate')}
-            {createInput('CVC', 'cvc')}
-          </Row>
-        </Row>
-        <Row margin={8} mainAxisAlignment="end" crossAxisAlignment="end">
-          <Button text="Buy" onPress={makeOrder} />
-        </Row>
+      <Column backgroundColor="#f2f2f2" flex={1}>
+        <ScrollView>
+          <Column padding={12}>
+            <Row marginBottom={12}>
+              <Column>
+                <PaymentLabel text="Card number" />
+                <PaymentInput placeholder="0000 0000 0000 0000" name="number" paymentState={card} />
+              </Column>
+            </Row>
+            <Row marginBottom={48}>
+              <Column marginEnd={6}>
+                <PaymentLabel text="Expiration Date" />
+                <PaymentInput placeholder="MM/YY" name="expirationDate" paymentState={card} />
+              </Column>
+              <Column marginStart={6}>
+                <PaymentLabel text="CVC" />
+                <PaymentInput placeholder="000" name="cvc" paymentState={card} />
+              </Column>
+            </Row>
+            <Row mainAxisAlignment="center">
+              <Button text="Finish Buy" onPress={makeOrder} />
+            </Row>
+          </Column>
+        </ScrollView>
       </Column>
     </ScreenComponent>
   )
