76b4af9706822a00028c7c228765da4ed45853ae
Tiago Peres França <tiago.franca@zup.com.br>
2022-12-19 14:41:04 -0300

    fixes operation types on number params

--- core/src/types.ts
+++ core/src/types.ts
@@ -42,11 +42,6 @@ export type InterpolatedText = InterpolatedTextAcceptableTypes | InterpolatedTex
 export type ValueOrResultOfExpression<T> = T extends State<infer R> ? R : (T extends Operation<infer R> ? R : T)
 
 /**
- * A string that represents a number
- */
-export type NumberString = `${number}`
-
-/**
  * A number or a string that represents a number
  */
 export type AnyNumber = number | `${number}`
