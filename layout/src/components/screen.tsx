243bdfe38f83e163b17027e784eb46b14a000bb7
Tiago Peres França <tiagoperesfr@gmail.com>
2023-01-06 11:41:40 -0300

    feat: status bar color (#35)
    * versions
    
    * adds properties for setting the status bar color
    
    * adds properties for setting the status bar color
    
    * fixes merge

--- layout/src/components/screen.tsx
+++ layout/src/components/screen.tsx
@@ -1,16 +1,19 @@
-import { FC, NimbusJSX, WithChildren, WithState } from '@zup-it/nimbus-backend-core'
+import { Expression, FC, NimbusJSX, WithChildren, WithState } from '@zup-it/nimbus-backend-core'
 import { genericNamespace } from '@zup-it/nimbus-backend-core/constants'
+import { Color } from '@zup-it/nimbus-backend-core/model/style/color'
 
 export interface ScreenProps extends WithState, Required<WithChildren> {
   /**
    * @default null|undefined|[]
    */
-  ignoreSafeArea?: ('top' | 'bottom' | 'trailing' | 'leading')[],
-  title?: string,
+  ignoreSafeArea?: Expression<('top' | 'bottom' | 'trailing' | 'leading')[]>,
+  title?: Expression<string>,
+  safeAreaTopBackground?: Expression<Color>,
+  statusBarColorScheme?: Expression<'light' | 'dark' | 'unspecified'>,
   /**
    * @default true
    */
-  showBackButton?: boolean,
+  showBackButton?: Expression<boolean>,
 }
 
 export const ScreenComponent: FC<ScreenProps> = ({ id, state, children, ...props }) => (
